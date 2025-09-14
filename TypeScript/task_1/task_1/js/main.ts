4ed6e96536bcc387ff9f92ab4013db7a4f91c74d
Alejandro <10653@holbertonstudents.com>
2025-09-14 18:21:14 -0300

    3. Printing teachers

--- TypeScript/task_1/task_1/js/main.ts
+++ TypeScript/task_1/task_1/js/main.ts
@@ -1,4 +1,4 @@
-// Task 1: Teacher interface
+// Task 1: Teacher Interface
 
 interface Teacher {
   readonly firstName: string;
@@ -6,20 +6,20 @@ interface Teacher {
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  [propName: string]: any;
+  [key: string]: any;
 }
 
 const teacher3: Teacher = {
   firstName: 'John',
-  lastName: 'Doe',
   fullTimeEmployee: false,
+  lastName: 'Doe',
   location: 'London',
   contract: false,
 };
 
 console.log(teacher3);
 
-// Task 2: Directors interface extending Teacher
+// Task 2: Directors Interface extends Teacher
 
 interface Directors extends Teacher {
   numberOfReports: number;
@@ -34,6 +34,17 @@ const director1: Directors = {
 };
 
 console.log(director1);
+
+// Task 3: printTeacher function and interface
+interface PrintTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: PrintTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+console.log(printTeacher("John", "Doe"));
 
 // should print:
 // object
